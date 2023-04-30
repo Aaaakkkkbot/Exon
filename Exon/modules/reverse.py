b66f7ffd594677585d009f2953bfc97e68bc3d03
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2023-04-30 08:56:17 +0530

    Update reverse.py

--- Exon/modules/reverse.py
+++ Exon/modules/reverse.py
@@ -10,7 +10,7 @@ from pyrogram import Client, filters
 from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, Message)
 import httpx
 
-from Exon import pbot
+from Exon import Exon
 
 API_URL = 'https://sasta.tk/google_reverse'
 
